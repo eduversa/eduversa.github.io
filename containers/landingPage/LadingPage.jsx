50065eaba4e566e48f23b8f5b85337502ee8b095
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-11 20:47:38 +0530

    Landing Page jsx added

---
+++ containers/landingPage/LadingPage.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import { TypingText } from "@/components";
+
+function LandingPage() {
+  return (
+    <Fragment>
+      <div className="homepage-left">
+        <h1 className="company-name">Welcome To UNIVERSA</h1>
+        <h2 className="company-slogan">Chase Your Dream</h2>
+        <div className="typing-text-container">
+          <main className="typing-text-main">
+            <TypingText />
+          </main>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default LandingPage;
