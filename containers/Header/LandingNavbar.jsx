03faf5f2a9691114c6dabc1a28a4856254bf7437
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-11 20:39:59 +0530

    updated Navbar

--- containers/Header/LandingNavbar.jsx
+++ containers/Header/LandingNavbar.jsx
@@ -7,25 +7,31 @@ function LandingNavbar() {
   return (
     <Fragment>
       <header>
-        <nav>
+        <nav className="navbar">
           <div className="logo">
-            <Link href="/">{logoText && <span>{logoText}</span>}</Link>
+            <Link href="/">
+              <a className="logo-link">{logoText && <span>{logoText}</span>}</a>
+            </Link>
           </div>
-          <div>
-            <ul>
-              <li>
-                <Link href="/about">About US</Link>
+          <div className="nav-section">
+            <ul className="nav-list">
+              <li className="nav-item">
+                <Link href="/about">
+                  <a>About US</a>
+                </Link>
               </li>
-              <li>
-                <Link href="/contact">Contact US</Link>
+              <li className="nav-item">
+                <Link href="/contact">
+                  <a>Contact US</a>
+                </Link>
               </li>
             </ul>
           </div>
-          <div>
-            <button>
+          <div className="button-section">
+            <button className="login-button">
               <Link href="/">Login</Link>
             </button>
-            <button>
+            <button className="register-button">
               <Link href="/register">Register</Link>
             </button>
           </div>
