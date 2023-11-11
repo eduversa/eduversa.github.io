862f96bab87657b33101babff0986abfdf103929
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-11 19:33:48 +0530

    Added landing layout

---
+++ layout/landing_layout/landingLayout.jsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react"
+function landingLayout({children}) {
+    return (
+        <Fragment>
+            {children}
+        </Fragment>
+    )
+}
+export default landingLayout
