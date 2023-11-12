f7d5ed4b2f5368be39131323c678814f2575180b
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-12 17:32:36 +0530

    initial login page created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,47 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { LandingLayout } from "@/layout";
 import { LandingPage } from "@/containers";
+
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
   return (
     <Fragment>
       <LandingLayout>
-        <h1>Login</h1>
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username:
+              <input
+                type="text"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+          </form>
+        </div>
       </LandingLayout>
     </Fragment>
   );
 }
-
 export default Login;
