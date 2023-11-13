e0333c63668ac26f7b95aa34678da8bac456e011
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-14 01:14:17 +0530

    register container is starting to make.

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,10 +1,54 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { LandingLayout } from "@/layout";
+import Image from "next/image";
+import Link from "next/link";
+
 function Register() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Registering...", email);
+  };
+
   return (
     <Fragment>
       <LandingLayout>
-        <h1>Register</h1>
+        <div className="register-container">
+          <h2 className="register-heading">Register</h2>
+          <form className="register-form" onSubmit={handleSubmit}>
+            <div className="register-email">
+              <Image
+                src="/register/email.png"
+                alt="email"
+                height={20}
+                width={20}
+                className="email-icon"
+              />
+              <input
+                type="email"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="email-input"
+                required
+              />
+            </div>
+
+            <div className="button-container">
+              <button type="submit" className="register-button">
+                Register
+              </button>
+            </div>
+
+            <div className="login-prompt">
+              <span>Already have an account?</span>
+              <Link href="/login">
+                <p className="login-link">Click here to Login</p>
+              </Link>
+            </div>
+          </form>
+        </div>
       </LandingLayout>
     </Fragment>
   );
