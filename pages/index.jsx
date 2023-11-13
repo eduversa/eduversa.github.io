4d04526ab7393ed183207b8edaf0dc8b753fa912
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-13 15:57:41 +0530

    newlogin container added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,52 @@
 import { Fragment, useState } from "react";
 import { LandingLayout } from "@/layout";
+import Image from "next/image";
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <Fragment>
       <LandingLayout>
-        <h1>Login</h1>
+        <div className="login-container">
+          <h2>Login</h2>
+          <form className="login-form">
+            <div className="input-group">
+              <Image
+                src="/login/username.png"
+                alt="username"
+                height={20}
+                width={20}
+              ></Image>
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="username-input"
+              />
+            </div>
+            <div className="input-group">
+              <Image
+                src="/login/password.png"
+                alt="username"
+                height={20}
+                width={20}
+              ></Image>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="password-input"
+              />
+            </div>
+            <button type="button" className="login-button">
+              Login
+            </button>
+          </form>
+        </div>
       </LandingLayout>
     </Fragment>
   );
 }
-
 export default Login;
