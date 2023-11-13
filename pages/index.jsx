cb4337bcdd765b22bb67f0f2a60d4d0ae5e6eae3
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-13 14:45:52 +0530

    login container resetted

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,60 +1,10 @@
 import { Fragment, useState } from "react";
 import { LandingLayout } from "@/layout";
-import Image from "next/image";
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Username:", username);
-    console.log("Password:", password);
-  };
-
   return (
     <Fragment>
       <LandingLayout>
-        <div className="login-container">
-          <h2 className="login-heading">Login</h2>
-          <form className="login-form" onSubmit={handleSubmit}>
-            <label className="form-label">
-              <Image
-                src="/login/username.png"
-                alt="Username"
-                height={20}
-                width={20}
-                className="input-icon"
-              ></Image>
-              <input
-                type="text"
-                placeholder="Enter your username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="form-input"
-              />
-            </label>
-            <label className="form-label">
-              <Image
-                src="/login/password.png"
-                alt="Password"
-                height={20}
-                width={20}
-                className="input-icon"
-              ></Image>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="form-input"
-              />
-            </label>
-            <br />
-            <button type="submit" className="login-button">
-              Login
-            </button>
-          </form>
-        </div>
+        <h1>Login</h1>
       </LandingLayout>
     </Fragment>
   );
