a8e21eae46be53708a17ef6e2c18933f2736378c
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-14 12:50:06 +0530

    minor bug fixed

--- containers/landingPage/LadingPage.jsx
+++ containers/landingPage/LadingPage.jsx
@@ -6,7 +6,7 @@ function LandingPage() {
     <Fragment>
       <div className="homepage-left">
         <h1 className="company-name">Welcome To UNIVERSA</h1>
-        <h2 className="company-slogan">Chase Your Dream</h2>
+        <h2 className="company-slogan">Chase Your Dream!</h2>
         <div className="typing-text-container">
           <main className="typing-text-main">
             <TypingText />
