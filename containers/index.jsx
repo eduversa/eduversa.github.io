d98583a48f8d1f059cd63a51a0ae0691e31a83d1
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-16 23:14:19 +0530

    Contact and about us page created

--- containers/index.jsx
+++ containers/index.jsx
@@ -2,3 +2,5 @@ export { default as LandingNavbar } from "./Header/LandingNavbar";
 export { default as LandingPage } from "./landingPage/LadingPage";
 export { default as ApplicantNavbar } from "./Header/ApplicantNavbar";
 export { default as Footer } from "./Footer/Footer";
+export { default as AboutUs } from "./aboutUs/AboutUs";
+export { default as ContactUs } from "./ContactUs/ContactUs";
