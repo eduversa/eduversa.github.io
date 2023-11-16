37d0b25b152a0c9b66149a563a1a6099f06f6a37
Tanay <tanayghoriwala001@gmail.com>
2023-11-16 19:13:04 +0530

    footer update

--- containers/Footer/Footer.jsx
+++ containers/Footer/Footer.jsx
@@ -1,45 +1,54 @@
-import React, { Fragment, useState } from "react";
-import Link from "next/link";
+import React, { Fragment, useEffect, useState } from 'react';
+
+const Footer = () => {
+  const [userType, setUserType] = useState(null);
+
+  useEffect(() => {
+   
+    const storedUserType = localStorage.getItem('userType');
+    setUserType(storedUserType);
+  }, []);
 
 
-function Footer() {
+  const footerContent = {
+    student: [
+      { label: 'Update ID Card', href: '/update-id-card' },
+      { label: 'Dashboard', href: '/dashboard' },
+    ],
+    applicant: [
+      { label: 'Dashboard', href: '/applicant' },
+      { label: 'Update Profile', href: '/applicant/update' },
+      { label: 'About Us', href: '/applicant/about' },
+      { label: 'Contact Us', href: '/applicant/contact' },
+    ],
+
+  };
+
+
+  const userFooterLinks = footerContent[userType];
   let year = new Date().getFullYear().toString();
 
-  const menuItems = [
-    { label: "Applicant", className: "nav-items", src: "/applicant" },
-    {
-      label: "Student",
-      className: "nav-items",
-      src: "/applicant",
-    },
-    { label: "Faculty", className: "nav-items", src: "/applicant" },
-    { label: "Admin", className: "nav-items", src: "/applicant" },
-  ];
-
-
   return (
-
     <Fragment>
-      <footer>
-        <nav className="footer-nav">
-          <div className="copyright">
+    <footer className='footer'>
+     
+    <div className="copyright">
             <p>&copy; {year} Eduversa. All Rights Reserved.</p>
           </div>
-          <div className="nav-links">
-          <ul className="nav-list">
-              {menuItems.map((item, index) => (
-                <li key={index} className={item.className}>
-                  <Link href={item.src}>
-                    <span>{item.label}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </nav>
-      </footer>
+      <ul>
+        {userFooterLinks ? (
+          userFooterLinks.map((link, index) => (
+            <li className= "nav-links" key={index}>
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))
+        ) : (
+          <li>User type &apos;{userType}&apos; not found in footerContent</li>
+        )}
+      </ul>
+    </footer>
     </Fragment>
-  )
-}
+  );
+};
 
 export default Footer;
