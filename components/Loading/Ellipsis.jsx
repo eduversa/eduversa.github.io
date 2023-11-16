3fb134cdb8e4ab9480c2f2a8be52802f37065e98
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-16 23:59:43 +0530

    Ellipsis added

---
+++ components/Loading/Ellipsis.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const Ellipsis = () => {
+  return (
+    <Fragment>
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Ellipsis;
