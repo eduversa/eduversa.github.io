d2dedcb06602a0950ba10dd51e4398e880ba7a9f
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-16 23:49:56 +0530

    updated LdsCircle

--- components/Loading/LdsCircle.jsx
+++ components/Loading/LdsCircle.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const LdsCircle = () => {
   return (
-    <div className="lds-circle">
-      <div></div>
-    </div>
+    <Fragment>
+      <div className="lds-circle">
+        <div></div>
+      </div>
+    </Fragment>
   );
 };
 
