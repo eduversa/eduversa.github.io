8e98af9a7d0bdbaaf50454268be5dfbbaff950e6
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-16 16:01:18 +0530

    all authentication is working all right

--- layout/applicant_layout/ApplicantLayout.jsx
+++ layout/applicant_layout/ApplicantLayout.jsx
@@ -1,6 +1,20 @@
-import { Fragment } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Fragment, useEffect } from "react";
+import { useRouter } from "next/router";
 import { ApplicantNavbar } from "@/containers";
 function ApplicantLayout() {
+  const router = useRouter();
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+    const userType = localStorage.getItem("userType");
+    console.log("AuthToken", authToken);
+    console.log("UserType", userType);
+    if (!authToken) {
+      router.push("/");
+    } else if (userType === "applicant") {
+      router.push("/applicant");
+    }
+  }, []);
   return (
     <Fragment>
       <ApplicantNavbar />
