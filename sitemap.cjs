11b955bb7f529dd5bf640d70d988e3a3c03e9f9c
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-17 02:37:34 +0530

    site map working

--- sitemap.cjs
+++ sitemap.cjs
@@ -1,27 +1,28 @@
-const pkg = require("sitemap");
-const { createSitemap } = pkg;
+const { SitemapStream, streamToPromise } = require("sitemap");
+const fs = require("fs");
 
-function generateSitemap() {
+async function generateSitemap() {
   console.log("Generating sitemap...");
 
   const baseUrl = "https://eduversa.github.io";
   const staticRoutes = ["/", "/about", "/contact", "/register", "/login"];
   const allRoutes = [...staticRoutes];
 
-  const sitemap = pkg.createSitemap({
-    hostname: baseUrl,
-    cacheTime: 600000,
-    urls: allRoutes.map((route) => ({
-      url: route,
-      changefreq: "daily",
-      priority: 0.7,
-    })),
+  const sitemapStream = new SitemapStream({ hostname: baseUrl });
+
+  allRoutes.forEach((route) => {
+    sitemapStream.write({ url: route, changefreq: "daily", priority: 0.7 });
   });
+
+  sitemapStream.end();
 
   const filePath = "./public/sitemap.xml";
 
   try {
-    pkg.writeFileSync(filePath, sitemap.toString());
+    const sitemapXml = await streamToPromise(sitemapStream).then((data) =>
+      data.toString()
+    );
+    fs.writeFileSync(filePath, sitemapXml);
     console.log("Sitemap successfully written to:", filePath);
   } catch (error) {
     console.error("Error writing sitemap:", error);
