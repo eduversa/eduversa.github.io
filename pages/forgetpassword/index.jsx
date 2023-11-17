91da7f346430019651476100f1a6e1199db035bd
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-17 14:11:04 +0530

    forget password page added

---
+++ pages/forgetpassword/index.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import { ApplicantLayout } from "@/layout";
+function ForgetPassword() {
+  return (
+    <Fragment>
+      <ApplicantLayout>
+        <h1>Forget Password</h1>
+      </ApplicantLayout>
+    </Fragment>
+  );
+}
+
+export default ForgetPassword;
