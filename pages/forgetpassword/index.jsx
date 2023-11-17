76c6e3c45c3f413740d3ebf6c80cd2b83d7e0511
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-17 14:49:47 +0530

    update forget page

--- pages/forgetpassword/index.jsx
+++ pages/forgetpassword/index.jsx
@@ -6,6 +6,7 @@ import { generateOtpApi } from "@/functions";
 function ForgetPassword() {
   const [inputValue, setInputValue] = useState("");
   const [loading, setLoading] = useState(false);
+  const [otpResponse, setOtpResponse] = useState(null);
 
   const isValidEmail = (value) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -57,27 +58,65 @@ function ForgetPassword() {
         {loading && <AllLoader />}
         <div className="forget-password-container">
           <h1>Forget Password</h1>
-          <div className="forget-password-subheading-container">
-            <h3 className="forget-password-subheading">
-              Enter your userId/Email address to generate OTP.
-            </h3>
-          </div>
-          <form className="forget-password-form" onSubmit={handleSubmit}>
-            <div className="forget-password-input-container">
-              <input
-                type="text"
-                className="forget-password-input"
-                placeholder="Enter your userId/Email address"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-            </div>
-            <div className="forget-password-button-container">
-              <button type="submit" className="forget-password-button">
-                Generate OTP
-              </button>
-            </div>
-          </form>
+          {otpResponse ? (
+            <>
+              <h3 className="forget-password-subheading">
+                Verify OTP and enter your new password!
+              </h3>
+              <form
+                className="new-password-form"
+                onSubmit={handleNewPasswordSubmit}
+              >
+                <div className="new-password-input-container">
+                  <input
+                    type="password"
+                    className="new-password-input"
+                    placeholder="New Password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </div>
+                <div className="confirm-password-input-container">
+                  <input
+                    type="password"
+                    className="confirm-password-input"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+                <div className="new-password-button-container">
+                  <button type="submit" className="new-password-button">
+                    Set New Password
+                  </button>
+                </div>
+              </form>
+            </>
+          ) : (
+            <>
+              <div className="forget-password-subheading-container">
+                <h3 className="forget-password-subheading">
+                  Enter your userId/Email address to generate OTP.
+                </h3>
+              </div>
+              <form className="forget-password-form" onSubmit={handleSubmit}>
+                <div className="forget-password-input-container">
+                  <input
+                    type="text"
+                    className="forget-password-input"
+                    placeholder="Enter your userId/Email address"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                  />
+                </div>
+                <div className="forget-password-button-container">
+                  <button type="submit" className="forget-password-button">
+                    Generate OTP
+                  </button>
+                </div>
+              </form>
+            </>
+          )}
         </div>
       </LandingLayout>
     </Fragment>
