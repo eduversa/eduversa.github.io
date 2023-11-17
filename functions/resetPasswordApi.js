2517c113bb313d254e9ab7fa130c4924ce025da8
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-17 20:58:18 +0530

    resetPasswordApi.js created

---
+++ functions/resetPasswordApi.js
@@ -0,0 +1,44 @@
+const BASE_URL = "https://eduversa-api.onrender.com";
+
+const resetPasswordApi = async (
+  userIdOrEmail,
+  otp,
+  newPassword,
+  confirmPassword
+) => {
+  const url = `${BASE_URL}/account/password?query=${userIdOrEmail}&otp=${otp}`;
+  console.log("URL:", url);
+
+  try {
+    console.log("Reset Password Function Called");
+    console.log("User ID or Email:", userIdOrEmail);
+
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password: newPassword,
+        confirm_password: confirmPassword,
+      }),
+    });
+
+    if (!response.ok) {
+      console.log(response);
+      throw new Error(
+        `Reset Password request failed with status ${response.status}`
+      );
+    }
+
+    console.log("Response:", response);
+    const data = await response.json();
+    console.log("Data:", data);
+    return data;
+  } catch (error) {
+    console.error("Reset Password request error:", error.message);
+    throw error;
+  }
+};
+
+export default resetPasswordApi;
