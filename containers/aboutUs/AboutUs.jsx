a3c502cc4da813ea0e59745d6ebefd3ce00791e1
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-18 18:35:58 +0530

    about.jsx updated

--- containers/aboutUs/AboutUs.jsx
+++ containers/aboutUs/AboutUs.jsx
@@ -1,43 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const aboutUs = require("../../data/aboutUs");
 
 function AboutUs() {
-  const generateProblemsContent = () => {
-    return Object.entries(aboutUs.problems).map(([key, value], index) => (
-      <div
-        key={index}
-        className={`problem-${key.toLowerCase().replace(/ /g, "-")}`}
-      >
-        {value}
-      </div>
-    ));
+  const [showProblems, setShowProblems] = useState(false);
+  const [showKeySolution, setShowKeySolution] = useState(false);
+  const [showFuturePlans, setShowFuturePlans] = useState(false);
+
+  const toggleSection = (section) => {
+    switch (section) {
+      case "problems":
+        setShowProblems(!showProblems);
+        break;
+      case "keySolution":
+        setShowKeySolution(!showKeySolution);
+        break;
+      case "futurePlans":
+        setShowFuturePlans(!showFuturePlans);
+        break;
+      default:
+        break;
+    }
   };
 
-  const generateKeySolutionAndObjectiveContent = () => {
-    return Object.entries(aboutUs.keySolutionAndObjective).map(
-      ([key, value], index) => (
+  const generateProblemsContent = () => {
+    if (showProblems) {
+      return Object.entries(aboutUs.problems).map(([key, value], index) => (
         <div
           key={index}
-          className={`key-solution-and-objective-${key
-            .toLowerCase()
-            .replace(/ /g, "-")}`}
+          className={`problem-${key.toLowerCase().replace(/ /g, "-")}`}
         >
           {value}
         </div>
-      )
-    );
+      ));
+    }
+    return null;
+  };
+
+  const generateKeySolutionAndObjectiveContent = () => {
+    if (showKeySolution) {
+      return Object.entries(aboutUs.keySolutionAndObjective).map(
+        ([key, value], index) => (
+          <div
+            key={index}
+            className={`key-solution-and-objective-${key
+              .toLowerCase()
+              .replace(/ /g, "-")}`}
+          >
+            {value}
+          </div>
+        )
+      );
+    }
+    return null;
   };
 
   const generateFuturePlansContent = () => {
-    return Object.entries(aboutUs.futurePlans).map(([key, value], index) => (
-      <div
-        key={index}
-        className={`future-plans-${key.toLowerCase().replace(/ /g, "-")}`}
-      >
-        {value}
-      </div>
-    ));
+    if (showFuturePlans) {
+      return Object.entries(aboutUs.futurePlans).map(([key, value], index) => (
+        <div
+          key={index}
+          className={`future-plans-${key.toLowerCase().replace(/ /g, "-")}`}
+        >
+          {value}
+        </div>
+      ));
+    }
+    return null;
   };
 
   return (
@@ -47,17 +76,19 @@ function AboutUs() {
       <div className="technologies">{aboutUs.technologies}</div>
 
       <div className="problems">
-        <h2>Problems</h2>
+        <h2 onClick={() => toggleSection("problems")}>Problems</h2>
         {generateProblemsContent()}
       </div>
 
       <div className="key-solution-and-objective">
-        <h2>Key Solution and Objective</h2>
+        <h2 onClick={() => toggleSection("keySolution")}>
+          Key Solution and Objective
+        </h2>
         {generateKeySolutionAndObjectiveContent()}
       </div>
 
       <div className="future-plans">
-        <h2>Future Plans</h2>
+        <h2 onClick={() => toggleSection("futurePlans")}>Future Plans</h2>
         {generateFuturePlansContent()}
       </div>
     </div>
