23f4793febb7a1e31e55de76c8b98d092f251765
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-18 18:06:12 +0530

    updated object

--- data/aboutUs.js
+++ data/aboutUs.js
@@ -1,13 +1,24 @@
 const aboutUs = {
-  overview: {
-    description:
-      "Eduversa represents an advanced college ERP system meticulously crafted to address the intricate data management needs of educational institutions. Functioning as an Enterprise Resource Planning (ERP) software, Eduversa is tailored to streamline processes related to staff, students, and other stakeholders within a college ecosystem.",
-    technology:
-      "Built on cutting-edge technologies, including ReactJs and MongoDB, and enhanced by the use of a pre-processor such as SCSS, Eduversa stands as a testament to modern, efficient, and user-centric design principles.",
+  description:
+    "Eduversa represents an advanced college ERP system meticulously crafted to address the intricate data management needs of educational institutions. Functioning as an Enterprise Resource Planning (ERP) software, Eduversa is tailored to streamline processes related to staff, students, and other stakeholders within a college ecosystem.",
+  technologies:
+    "Built on cutting-edge technologies, including ReactJs and MongoDB, and enhanced by the use of a pre-processor such as SCSS, Eduversa stands as a testament to modern, efficient, and user-centric design principles.",
+
+  problems: {
+    userAdoption: "User Adoption Hindered by Persistent Bugs and Glitches",
+    compatibilityIssues: "Mobile Compatibility Issues",
+    scheduleManagement:
+      "This is something that a teacher might appreciate the most. Imagine you had one single place for your data regarding your college and also all your classes mentioned within the same data-set for ease of access.",
+    communicationUpdates:
+      "Currently to link the connection from a teacher to a student, one would have to use a chatting software dedicatedly for the sole purpose of communication. What if there was a feature that would allow us to make announcements that would be declared within the chosen class.",
+    lackOfVisualData:
+      "It is essential to have a feature that can visually represent the data regarding the attendance of the student, the performance of the student and other such things. These sorts of improvements can bring out great changes within the college, students aware of where they stand in the class and their personal performance will help them with growing into becoming someone better.",
+    lowRateOfInteraction:
+      "As there is not much that us students can do in order to contact a teacher, it would be easier if the students could directly text the teacher from the software itself and the teacher can reply according to their availability. This removes the issue for the teachers to rethink the legitimacy of the student’s identity.",
   },
-  keyObjectivesAndSolutions: {
+  keySolutionAndObjective: {
     userFriendlyUI:
-      "Eduversa prioritizes a user-friendly interface, ensuring intuitive functionality and robust management systems to enhance the user experience.",
+      "Eduversa prioritizes a user-friendly interface, ensuring intuitive functionality and robust management systems to enhance the user experience. Eduversa is a fully responsive design, enabling users to access Eduversa seamlessly across diverse devices, fostering accessibility.",
     centralizedInformation:
       "The system seeks to centralize all college-related information under a unified framework, promoting transparency and efficiency in academic processes.",
     digitalAdministrationAndTeacherAuthority:
@@ -18,9 +29,10 @@ const aboutUs = {
       "Implemented a nuanced access control system, tailoring permissions for administrators, teachers, and students based on their distinct roles and responsibilities.",
     studentPortal:
       "Eduversa boasts a dedicated student portal, offering registered students seamless access to all necessary data anytime, anywhere.",
-    adminPortal:
+    comprehensiveControl:
       "The administrative portal provides comprehensive control, allowing administrators to enroll new applicants and manage essential requirements with utmost authority.",
   },
+
   futurePlans: {
     description:
       "Eduversa is not just a system; it's a dynamic and evolving solution. Continuously incorporating user feedback, staying technologically updated, and adapting to the ever-changing educational landscape are paramount to its success.Future plans.",
@@ -44,6 +56,3 @@ const aboutUs = {
       "Ensuring that the platform is more accessible by incorporating features like screen reader compatibility and keyboard navigation.",
   },
 };
-
-console.log(aboutUs.keyObjectivesAndSolutions.userFriendlyUI);
-console.log(aboutUs.futurePlans.digitalAttendanceAndClassRoutine);
