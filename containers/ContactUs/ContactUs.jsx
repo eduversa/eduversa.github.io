0887371130890b858e403041d9e1859c96d75071
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-21 18:41:48 +0530

    updated jsx

--- containers/ContactUs/ContactUs.jsx
+++ containers/ContactUs/ContactUs.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from "react";
+import React from "react";
+
 const contactUs = require("../../data/ContactUs");
 const ContactUs = () => {
   const { teamMembers, contactEmail, teamObjective, additionalInformation } =
@@ -15,68 +16,72 @@ const ContactUs = () => {
     ));
   };
 
+  const authToken =
+    typeof window !== "undefined" ? localStorage.getItem("authToken") : null;
+  const containerClass = authToken
+    ? "contact-us-container"
+    : "contact-us-landing-container";
+
   return (
-    <Fragment>
-      <div className="contact-us-container">
-        <h2 className="contact-us-heading">Contact Us</h2>
+    <div className={containerClass}>
+      <h2 className="contact-us-heading">Contact Us</h2>
 
-        <div className="team-members">
-          <h3 className="team-members-heading">Our Team</h3>
-          {renderTeamMembers()}
-        </div>
+      <div className="team-members">
+        <h3 className="team-members-heading">Our Team</h3>
+        {renderTeamMembers()}
+      </div>
 
-        <div className="contact-email">
-          <h3 className="contact-email-heading">Contact Email</h3>
-          <p>{contactEmail}</p>
-        </div>
+      <div className="contact-email">
+        <h3 className="contact-email-heading">Contact Email</h3>
+        <p>{contactEmail}</p>
+      </div>
 
-        <div className="team-objective">
-          <h3 className="team-objective-heading">Team Objective</h3>
-          <p>{teamObjective}</p>
-        </div>
+      <div className="team-objective">
+        <h3 className="team-objective-heading">Team Objective</h3>
+        <p>{teamObjective}</p>
+      </div>
 
-        <div className="additional-information">
-          <h3 className="additional-information-heading">
-            Additional Information
-          </h3>
-          <div className="social-media">
-            <h4 className="social-media-heading">Social Media</h4>
-            <ul>
-              <li>
-                Twitter:
-                <a
-                  href={additionalInformation.socialMedia.twitter}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {additionalInformation.socialMedia.twitter}
-                </a>
-              </li>
-              <li>
-                LinkedIn:
-                <a
-                  href={additionalInformation.socialMedia.linkedin}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {additionalInformation.socialMedia.linkedin}
-                </a>
-              </li>
-              <li>
-                GitHub:
-                <a
-                  href={additionalInformation.socialMedia.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {additionalInformation.socialMedia.github}
-                </a>
-              </li>
-            </ul>
-          </div>
+      <div className="additional-information">
+        <h3 className="additional-information-heading">
+          Additional Information
+        </h3>
+        <div className="social-media">
+          <h4 className="social-media-heading">Social Media</h4>
+          <ul>
+            <li>
+              Twitter:{" "}
+              <a
+                href={additionalInformation.socialMedia.twitter}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {additionalInformation.socialMedia.twitter}
+              </a>
+            </li>
+            <li>
+              LinkedIn:{" "}
+              <a
+                href={additionalInformation.socialMedia.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {additionalInformation.socialMedia.linkedin}
+              </a>
+            </li>
+            <li>
+              GitHub:{" "}
+              <a
+                href={additionalInformation.socialMedia.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {additionalInformation.socialMedia.github}
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    </Fragment>
+    </div>
   );
 };
 
