8725c08bc9f6593a47963bdef79b587b5fc454dd
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-21 18:48:35 +0530

    Contact Us jsx created

--- containers/ContactUs/ContactUs.jsx
+++ containers/ContactUs/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const contactUs = require("../../data/ContactUs");
 const ContactUs = () => {
@@ -9,9 +9,15 @@ const ContactUs = () => {
     return teamMembers.map((member, index) => (
       <div key={index} className={`team-member-${index + 1}`}>
         <h3 className={`team-member-${index + 1}-heading`}>{member.name}</h3>
-        <p>Email: {member.email}</p>
-        <p>Expertise: {member.expertise}</p>
-        <p>{member.description}</p>
+        <p className={`team-member-${index + 1}-email`}>
+          Email: {member.email}
+        </p>
+        <p className={`team-member-${index + 1}-expertise`}>
+          Expertise: {member.expertise}
+        </p>
+        <p className={`team-member-${index + 1}-description`}>
+          {member.description}
+        </p>
       </div>
     ));
   };
@@ -23,65 +29,70 @@ const ContactUs = () => {
     : "contact-us-landing-container";
 
   return (
-    <div className={containerClass}>
-      <h2 className="contact-us-heading">Contact Us</h2>
+    <Fragment>
+      <div className={containerClass}>
+        <h2 className="contact-us-heading">Contact Us</h2>
 
-      <div className="team-members">
-        <h3 className="team-members-heading">Our Team</h3>
-        {renderTeamMembers()}
-      </div>
+        <div className="team-members">
+          <h3 className="team-members-heading">Our Team</h3>
+          {renderTeamMembers()}
+        </div>
 
-      <div className="contact-email">
-        <h3 className="contact-email-heading">Contact Email</h3>
-        <p>{contactEmail}</p>
-      </div>
+        <div className="contact-email">
+          <h3 className="contact-email-heading">Contact Email</h3>
+          <p className="contact-email-content">{contactEmail}</p>
+        </div>
 
-      <div className="team-objective">
-        <h3 className="team-objective-heading">Team Objective</h3>
-        <p>{teamObjective}</p>
-      </div>
+        <div className="team-objective">
+          <h3 className="team-objective-heading">Team Objective</h3>
+          <p className="team-objective-content">{teamObjective}</p>
+        </div>
 
-      <div className="additional-information">
-        <h3 className="additional-information-heading">
-          Additional Information
-        </h3>
-        <div className="social-media">
-          <h4 className="social-media-heading">Social Media</h4>
-          <ul>
-            <li>
-              Twitter:{" "}
-              <a
-                href={additionalInformation.socialMedia.twitter}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {additionalInformation.socialMedia.twitter}
-              </a>
-            </li>
-            <li>
-              LinkedIn:{" "}
-              <a
-                href={additionalInformation.socialMedia.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {additionalInformation.socialMedia.linkedin}
-              </a>
-            </li>
-            <li>
-              GitHub:{" "}
-              <a
-                href={additionalInformation.socialMedia.github}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {additionalInformation.socialMedia.github}
-              </a>
-            </li>
-          </ul>
+        <div className="additional-information">
+          <h3 className="additional-information-heading">
+            Additional Information
+          </h3>
+          <div className="social-media">
+            <h4 className="social-media-heading">Social Media</h4>
+            <ul>
+              <li>
+                Twitter:{" "}
+                <a
+                  href={additionalInformation.socialMedia.twitter}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="social-media-link"
+                >
+                  {additionalInformation.socialMedia.twitter}
+                </a>
+              </li>
+              <li>
+                LinkedIn:{" "}
+                <a
+                  href={additionalInformation.socialMedia.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="social-media-link"
+                >
+                  {additionalInformation.socialMedia.linkedin}
+                </a>
+              </li>
+              <li>
+                GitHub:{" "}
+                <a
+                  href={additionalInformation.socialMedia.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="social-media-link"
+                >
+                  {additionalInformation.socialMedia.github}
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
