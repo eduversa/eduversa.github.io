f03ad35e7aea496e116776243ac998731508300a
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-21 17:52:07 +0530

    all apis has been exported

--- functions/index.jsx
+++ functions/index.jsx
@@ -4,3 +4,13 @@ export { default as logoutApi } from "./logoutApi";
 export { default as generateOtpApi } from "./generateOtp";
 export { default as resetPasswordApi } from "./resetPasswordApi";
 export { default as resetUserNameApi } from "./resetUserNameApi";
+
+export { default as deleteApplicantsByYearApi } from "./deleteApplicantsByYearApi";
+
+export { default as deleteSingleApplicantApi } from "./deleteSingleApplicantApi";
+
+export { default as getApplicantsByYearApi } from "./getApplicantsByYearApi";
+
+export { default as getCollegeDetailsApi } from "./getCollegeDetailsApi";
+
+export { default as getSingleApplicantApi } from "./getSingleApplicantApi";
