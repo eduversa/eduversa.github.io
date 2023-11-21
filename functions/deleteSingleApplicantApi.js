90015a6d489c09c09326ee19da62f16549f1b71f
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-21 17:28:18 +0530

    delete sigle applicant added

---
+++ functions/deleteSingleApplicantApi.js
@@ -0,0 +1,35 @@
+const BASE_URL = "https://eduversa-api.onrender.com";
+
+const deleteApplicantApi = async (userId) => {
+  const url = `${BASE_URL}/applicant/?user_id=${userId}`;
+  console.log("URL:", url);
+
+  try {
+    console.log("Delete Applicant Function Called");
+    console.log("User ID:", userId);
+
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      console.log(response);
+      throw new Error(
+        `Delete Applicant request failed with status ${response.status}`
+      );
+    }
+
+    console.log("Response:", response);
+    const data = await response.json();
+    console.log("Data:", data);
+    return data;
+  } catch (error) {
+    console.error("Delete Applicant request error:", error.message);
+    throw error;
+  }
+};
+
+export default deleteApplicantApi;
