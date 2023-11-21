185c11d4bf683082b9a599055bb4ee8963d1c0bd
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-21 18:20:40 +0530

    contact us data created

--- containers/ContactUs/ContactUs.jsx
+++ containers/ContactUs/ContactUs.jsx
@@ -3,6 +3,23 @@ function ContactUs() {
   return (
     <Fragment>
       <h1>Contact Us</h1>
+      We are a team of 6 members, with each having expertise in different
+      departments. We are a team which loves and adores making new stuff with an
+      extra advantages of solving problems. - Ankur Halder (Leader) – email:
+      ankur.halder12345@gmail.com, expertise in both frontend and backend
+      technologies, someone who believes in constant learning and likes
+      developing products. - Vidit Modi – email:viditmodi2207@gmail.com ,
+      expertise in both frontend and backend technologies, has a deeprooted
+      belief in learning and experimenting new technologies by making great
+      products. - Debargha Mondal – email:debargha10000@gmail.com , learning and
+      growing in frontend technologies ; learner who fancies some affinity
+      towards making products look good. - Tanay Ghoriwala – email:
+      tanayghoriwala001@gmail.com,learning and growing in frontend technologies
+      ; learner who fancies both developing and problem solving. - Ankan Basak -
+      email: ankanbasak377@gmail.com , beginner. - Shreyasi Roy – email ,
+      beginner. We can be contacted using our email id -
+      eduversa.developer@gmail.com We as a team are determined to make this
+      project evolve and be at par with other similar state-of-art-facilities.
     </Fragment>
   );
 }
