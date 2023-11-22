860421877fda09279c91fd38168f32167a62dde2
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-23 01:58:58 +0530

    Major problem Created

--- pages/applicant/update/index.jsx
+++ pages/applicant/update/index.jsx
@@ -1,14 +1,11 @@
 import { Fragment } from "react";
 import { ApplicantLayout } from "@/layout";
 // import { ApplicantForm } from "@/components";
-import { UpdateForm } from "@/containers";
+
 function UpdateApplicant() {
   return (
     <Fragment>
-      <ApplicantLayout>
-        <UpdateForm></UpdateForm>
-        {/* <ApplicantForm /> */}
-      </ApplicantLayout>
+      <ApplicantLayout>{/* <ApplicantForm /> */}</ApplicantLayout>
     </Fragment>
   );
 }
