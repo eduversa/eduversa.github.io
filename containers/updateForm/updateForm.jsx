78537db0691da31109937802ca49c188bf243eed
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-23 00:06:48 +0530

    updated layout

--- containers/updateForm/updateForm.jsx
+++ containers/updateForm/updateForm.jsx
@@ -3,36 +3,47 @@ import PersonalInfoForm from "./PersonalInfo";
 import AcademicInfoForm from "./AcademicInfo";
 import FamilyInfoForm from "./FamilyInfo";
 import ApplicantCourseForm from "./ApplicantCourse";
+
 function UpdateFormLayout({ children }) {
   const [currentStep, setCurrentStep] = useState(0);
+  const [formData, setFormData] = useState({});
 
   const handlePrevious = () => {
     setCurrentStep((prevStep) => Math.max(0, prevStep - 1));
   };
 
-  const handleNext = () => {
+  const handleNext = (stepData) => {
+    setFormData((prevFormData) => ({ ...prevFormData, ...stepData }));
     setCurrentStep((prevStep) => prevStep + 1);
   };
 
   const handleSave = () => {
     console.log("Form saved!");
+    console.log("All Form Data:", formData);
   };
 
   const handleClear = () => {
     console.log("Form cleared!");
+    setFormData({});
+    setCurrentStep(0);
   };
 
   return (
     <Fragment>
       {React.Children.map(children, (child, index) =>
-        index === currentStep ? child : null
+        index === currentStep
+          ? React.cloneElement(child, {
+              onNext: handleNext,
+              formData: formData,
+            })
+          : null
       )}
 
       <button onClick={handlePrevious} disabled={currentStep === 0}>
         Previous
       </button>
       <button
-        onClick={handleNext}
+        onClick={() => handleNext(formData)} // Pass current form data to the next step
         disabled={currentStep === React.Children.count(children) - 1}
       >
         Next
@@ -45,10 +56,10 @@ function UpdateFormLayout({ children }) {
 
 const UpdateForm = () => (
   <UpdateFormLayout>
-    <PersonalInfoForm></PersonalInfoForm>
-    <AcademicInfoForm></AcademicInfoForm>
-    <FamilyInfoForm></FamilyInfoForm>
-    <ApplicantCourseForm></ApplicantCourseForm>
+    <PersonalInfoForm />
+    <AcademicInfoForm />
+    <FamilyInfoForm />
+    <ApplicantCourseForm />
   </UpdateFormLayout>
 );
 
