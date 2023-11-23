c3abe52f8ed39913449ba9a9b59b479ca0035c41
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-23 11:52:02 +0530

    functions restructured

--- components/ApplicantForm/formPages/PersonalInfo.jsx
+++ components/ApplicantForm/formPages/PersonalInfo.jsx
@@ -1,12 +1,28 @@
-import React,{useState, useEffect, Fragment} from "react";
-import { Text, Email, Number, Select, DateInput, Pincode, Aadhar, Pan, PhoneNumber } from "../inputComponent/InputComponent";
+import React, { useState, useEffect, Fragment } from "react";
+import {
+  Text,
+  Email,
+  Number,
+  Select,
+  DateInput,
+  Pincode,
+  Aadhar,
+  Pan,
+  PhoneNumber,
+} from "../inputComponent/InputComponent";
 import fetchAddressFromPincode from "../inputComponent/fetchAddressFromPincode";
 
-
-const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentPincodeError, permanentPincodeError, setPermanentPincodeError }) => {
+const PersonalInfo = ({
+  formData,
+  handleChange,
+  presentPincodeError,
+  setPresentPincodeError,
+  permanentPincodeError,
+  setPermanentPincodeError,
+}) => {
   const copyAddress = (e) => {
     const isChecked = e.target.checked;
-  
+
     let updatedFormData;
     if (isChecked) {
       // Copy present address to permanent address
@@ -16,8 +32,8 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
         personal_info: {
           ...formData.personal_info,
           permanent_address: { ...presentAddress },
-          are_addresses_same: true
-        }
+          are_addresses_same: true,
+        },
       };
     } else {
       // Clear the permanent address fields
@@ -26,30 +42,36 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
         personal_info: {
           ...formData.personal_info,
           permanent_address: {
-            street: '',
-            pincode: '',
-            city: '',
-            district: '',
-            state: ''
+            street: "",
+            pincode: "",
+            city: "",
+            district: "",
+            state: "",
           },
-          are_addresses_same: false
-        }
+          are_addresses_same: false,
+        },
       };
     }
-    handleChange({ target: { name: 'formData', value: updatedFormData } });
+    handleChange({ target: { name: "formData", value: updatedFormData } });
   };
-  
 
   const [presentPincode, setPresentPincode] = useState("");
   const [permanentPincode, setPermanentPincode] = useState("");
-  
+
   // const [presentPincodeError, setPresentPincodeError] = useState(false);
   // const [permanentPincodeError, setPermanentPincodeError] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
     if (presentPincode.length === 6) {
-      fetchAddressFromPincode(formData, handleChange, 'personal_info.present_address', presentPincode, setPresentPincodeError, controller);
+      fetchAddressFromPincode(
+        formData,
+        handleChange,
+        "personal_info.present_address",
+        presentPincode,
+        setPresentPincodeError,
+        controller
+      );
     }
     return () => {
       controller.abort();
@@ -59,16 +81,25 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
   useEffect(() => {
     const controller = new AbortController();
     if (permanentPincode.length === 6) {
-      fetchAddressFromPincode(formData, handleChange, 'personal_info.permanent_address', permanentPincode, setPermanentPincodeError, controller);
+      fetchAddressFromPincode(
+        formData,
+        handleChange,
+        "personal_info.permanent_address",
+        permanentPincode,
+        setPermanentPincodeError,
+        controller
+      );
     }
     return () => {
       controller.abort();
     };
   }, [permanentPincode, setPermanentPincodeError, formData, handleChange]);
 
-
+  const personaInfo = localStorage.setItem(
+    "personal_info",
+    JSON.stringify(formData.personal_info)
+  );
   return (
-
     <Fragment>
       {/* name */}
       <Text
@@ -78,7 +109,9 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
         onChange={handleChange}
         required
       />
-      <div className="grid-col-2"> {/* email contact */}
+      <div className="grid-col-2">
+        {" "}
+        {/* email contact */}
         <Email
           label="Email"
           name="personal_info.email"
@@ -86,16 +119,17 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           onChange={handleChange}
           required
         />
-
         <PhoneNumber
           label="Contact Number"
           name="personal_info.contact"
           value={formData.personal_info.contact}
           onChange={handleChange}
           required
-        />          
+        />
       </div>
-      <div className="grid-col-2"> {/* gender dob */}
+      <div className="grid-col-2">
+        {" "}
+        {/* gender dob */}
         <Select
           label="Gender"
           name="personal_info.gender"
@@ -103,13 +137,12 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           onChange={handleChange}
           required
           options={[
-            {key: "Select Gender", value: ""},
-            {key: "Male", value: "Male"},
-            {key: "Female", value: "Female"},
-            {key: "Other", value: "Other"},
+            { key: "Select Gender", value: "" },
+            { key: "Male", value: "Male" },
+            { key: "Female", value: "Female" },
+            { key: "Other", value: "Other" },
           ]}
         />
-
         <DateInput
           label="Date of Birth"
           name="personal_info.dob"
@@ -118,7 +151,9 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           required
         />
       </div>
-      <div className="grid-col-2"> {/* category blood_group */}
+      <div className="grid-col-2">
+        {" "}
+        {/* category blood_group */}
         <Select
           label="Category"
           name="personal_info.category"
@@ -126,16 +161,15 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           onChange={handleChange}
           required
           options={[
-            {key: "Select Category", value:""},
-            {key: "General", value: "GN"},
-            {key: "Scheduled Caste", value: "SC"},
-            {key: "Scheduled Tribe", value: "ST"},
-            {key: "Other Backward Classes", value: "OBC"},
-            {key: "Economically Weaker Section", value: "EWS"},
-            {key: "Other", value: "Other"},
+            { key: "Select Category", value: "" },
+            { key: "General", value: "GN" },
+            { key: "Scheduled Caste", value: "SC" },
+            { key: "Scheduled Tribe", value: "ST" },
+            { key: "Other Backward Classes", value: "OBC" },
+            { key: "Economically Weaker Section", value: "EWS" },
+            { key: "Other", value: "Other" },
           ]}
         />
-
         <Select
           label="Blood Group"
           name="personal_info.blood_group"
@@ -143,28 +177,28 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           onChange={handleChange}
           required
           options={[
-            {key: "Select Blood Group", value:""},
-            {key: "A+", value:"A+"},
-            {key: "A-", value:"A-"},
-            {key: "B+", value:"B+"},
-            {key: "B-", value:"B-"},
-            {key: "AB+", value:"AB+"},
-            {key: "AB-", value:"AB-"},
-            {key: "O+", value:"O+"},
-            {key: "O-", value:"O-"},
+            { key: "Select Blood Group", value: "" },
+            { key: "A+", value: "A+" },
+            { key: "A-", value: "A-" },
+            { key: "B+", value: "B+" },
+            { key: "B-", value: "B-" },
+            { key: "AB+", value: "AB+" },
+            { key: "AB-", value: "AB-" },
+            { key: "O+", value: "O+" },
+            { key: "O-", value: "O-" },
           ]}
         />
       </div>
-      <div className="grid-col-2"> {/* aadhar pan */}
-        <Aadhar 
+      <div className="grid-col-2">
+        {" "}
+        {/* aadhar pan */}
+        <Aadhar
           label="Aadhar Number"
           name="personal_info.aadhar_number"
           value={formData.personal_info.aadhar_number}
           onChange={handleChange}
           required
-
         />
-
         <Pan
           label="PAN Number"
           name="personal_info.pan_number"
@@ -175,9 +209,7 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           maxLength="10"
         />
       </div>
-      
       <hr />
-
       <h3 className="sub-heading">Present Address</h3> {/* present address */}
       <Text
         label="Street"
@@ -186,7 +218,9 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
         onChange={handleChange}
         required
       />
-      <div className="grid-col-2"> {/* pin city */}
+      <div className="grid-col-2">
+        {" "}
+        {/* pin city */}
         <Pincode
           label="Pincode"
           name="personal_info.present_address.pincode"
@@ -202,7 +236,6 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           // ref={input => input && input.setCustomValidity(presentPincodeError ? 'Invalid pincode' : '')}
           className={presentPincodeError ? "invalid" : ""}
         />
-
         <Text
           label="City"
           name="personal_info.present_address.city"
@@ -211,7 +244,9 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           required
         />
       </div>
-      <div className="grid-col-2"> {/* district state */}
+      <div className="grid-col-2">
+        {" "}
+        {/* district state */}
         <Text
           label="District"
           name="personal_info.present_address.district"
@@ -219,7 +254,6 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           onChange={handleChange}
           required
         />
-
         <Text
           label="State"
           name="personal_info.present_address.state"
@@ -228,12 +262,11 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           required
         />
       </div>
-      
       <hr />
-
-      <h3 className="sub-heading">Permanent Address</h3> {/* permanent address */}
+      <h3 className="sub-heading">Permanent Address</h3>{" "}
+      {/* permanent address */}
       {/* is permanent address same as present address */}
-      <div> 
+      <div>
         <label>
           <input
             type="checkbox"
@@ -250,7 +283,9 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
         value={formData.personal_info.permanent_address.street}
         onChange={handleChange}
       />
-      <div className="grid-col-2"> {/* pin city */}
+      <div className="grid-col-2">
+        {" "}
+        {/* pin city */}
         <Pincode
           label="Pincode"
           name="personal_info.permanent_address.pincode"
@@ -269,7 +304,9 @@ const PersonalInfo = ({ formData, handleChange, presentPincodeError, setPresentP
           onChange={handleChange}
         />
       </div>
-      <div className="grid-col-2"> {/* district state */}
+      <div className="grid-col-2">
+        {" "}
+        {/* district state */}
         <Text
           label="District"
           name="personal_info.permanent_address.district"
