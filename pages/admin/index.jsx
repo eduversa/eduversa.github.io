8401fb530ba3acc931fff079f181ee0c3602d588
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-23 15:56:02 +0530

    build bug removed

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,12 +1,14 @@
-import { AdminLayout } from '@/layout'
-import React, { Fragment } from 'react'
+import { AdminLayout } from "@/layout";
+import React, { Fragment } from "react";
 
 const adminDashboard = () => {
   return (
     <Fragment>
-      <AdminLayout></AdminLayout>
+      <AdminLayout>
+        <h1>This is Admin Dashboard</h1>
+      </AdminLayout>
     </Fragment>
-  )
-}
+  );
+};
 
-export default adminDashboard
+export default adminDashboard;
