36767f19349fce380871dfc67aab080bdf16a003
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-23 13:13:19 +0530

    admin layout updated

--- layout/admin_layout/AdminLayout.jsx
+++ layout/admin_layout/AdminLayout.jsx
@@ -1,14 +1,29 @@
-import {AdminNavbar, Footer} from '@/containers'
-import React, { Fragment } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { Fragment, useEffect } from "react";
+import { useRouter } from "next/router";
+import { AdminNavbar, Footer } from "@/containers";
 
-const AdminLayout = ({children}) => {
+function AdminLayout({ children }) {
+  const router = useRouter();
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+    const userType = localStorage.getItem("userType");
+    console.log("AuthToken", authToken);
+    console.log("UserType", userType);
+    if (!authToken) {
+      router.push("/");
+      if (userType !== "applicant") {
+        localStorage.removeItem("authToken");
+      }
+    }
+  }, []);
   return (
     <Fragment>
       <AdminNavbar></AdminNavbar>
       {children}
       <Footer></Footer>
     </Fragment>
-  )
+  );
 }
 
-export default AdminLayout
+export default AdminLayout;
