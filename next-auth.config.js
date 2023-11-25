bff52fd6bd24efad55347cd3ce6b82ec613aaf8f
Ankur Halder <ankur.halder12345@gmail.com>
2023-11-25 17:54:06 +0530

    bug fixed

--- next-auth.config.js
+++ next-auth.config.js
@@ -12,6 +12,7 @@ const nextAuthConfig = {
       profileUrl: "https://www.googleapis.com/oauth2/v1/userinfo",
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     },
   ],
 };
