358e2f662eea961f7068668705b85dd8d8265d42
Ankur Halder <ankur.halder12345@gmail.com>
2024-04-05 10:32:08 +0530

    in admin dashboard temp logout functionality added by clearing cookies

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,14 +1,19 @@
 import { AdminLayout } from "@/layout";
 import React, { Fragment } from "react";
 
-const adminDashboard = () => {
+const AdminDashboard = () => {
+  function logoutHandler() {
+    localStorage.clear();
+  }
+
   return (
     <Fragment>
       <AdminLayout>
         <h1>This is Admin Dashboard</h1>
+        <button onClick={logoutHandler}>logout</button>{" "}
       </AdminLayout>
     </Fragment>
   );
 };
 
-export default adminDashboard;
+export default AdminDashboard;
