e8e520e8d39f26a8ec46016da4c2a4ca3c00cf9a
Ankur Halder <ankur.halder12345@gmail.com>
2024-04-05 13:35:45 +0530

    Refactor AdminDashboard component and remove logoutHandler function

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -2,15 +2,10 @@ import { AdminLayout } from "@/layout";
 import React, { Fragment } from "react";
 
 const AdminDashboard = () => {
-  function logoutHandler() {
-    localStorage.clear();
-  }
-
   return (
     <Fragment>
       <AdminLayout>
-        <h1>This is Admin Dashboard</h1>
-        <button onClick={logoutHandler}>logout</button>{" "}
+        <h1>Admin Dashboard</h1>
       </AdminLayout>
     </Fragment>
   );
