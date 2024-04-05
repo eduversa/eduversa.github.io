b57161cda762e19f737a428279aa8e651ac88f4b
Ankur Halder <ankur.halder12345@gmail.com>
2024-04-05 10:52:01 +0530

    Refactor Navbar component to handle logout functionality

--- containers/Header/Navbar.jsx
+++ containers/Header/Navbar.jsx
@@ -1,5 +1,46 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { AllLoader } from "@/components";
+import { logoutApi } from "@/functions";
 function Navbar() {
-  return <div></div>;
+  const router = useRouter();
+  const logoText = "eduversa";
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [userType, setUserType] = useState(null);
+
+  //@ Its Handling the logout functionality
+  const handleLogout = async () => {
+    const userId = localStorage.getItem("userid");
+    const authToken = localStorage.getItem("authToken");
+
+    try {
+      setIsLoading(true);
+      const apiResponse = await logoutApi(userId, authToken);
+      if (apiResponse.status === false) {
+        alert(apiResponse.message);
+        setIsLoading(false);
+        return;
+      }
+      if (process.env.NODE_ENV === "development") {
+        console.log("Logout data:", apiResponse);
+      }
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("email");
+      localStorage.removeItem("userType");
+      localStorage.removeItem("userid");
+      localStorage.clear();
+      alert(apiResponse.message);
+      setIsLoading(false);
+      router.push("/");
+    } catch (error) {
+      if (process.env.NODE_ENV === "development") {
+        console.error("Logout error:", error.message);
+      }
+    }
+  };
+  return <Fragment></Fragment>;
 }
 
 export default Navbar;
