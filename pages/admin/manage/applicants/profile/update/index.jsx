52e657f86ab443cfd6f12fa4a3d480adc230f685
Ankur Halder <ankur.halder12345@gmail.com>
2024-04-09 22:28:50 +0530

    Refactor UpdateApplicant component to remove unused code and fix localStorage access

--- pages/admin/manage/applicants/profile/update/index.jsx
+++ pages/admin/manage/applicants/profile/update/index.jsx
@@ -1,13 +1,16 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect } from "react";
 import { AdminLayout } from "@/layout";
-import { FormLayout } from "@/containers";
 import { ApplicantForm } from "@/components";
 
 const UpdateApplicant = () => {
-  const userid = localStorage.getItem("selected-applicantId");
   useEffect(() => {
     console.log("HI, Debargha", localStorage.getItem("selected-applicantId"));
   }, []);
+
+  const userid =
+    typeof window !== "undefined"
+      ? localStorage.getItem("selected-applicantId")
+      : null;
 
   return (
     <Fragment>
