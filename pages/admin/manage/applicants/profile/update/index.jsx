28f30b239f190882676e01ab84955fc81dd784d5
Ankur Halder <ankur.halder12345@gmail.com>
2024-04-09 22:21:46 +0530

    Refactor applicant profile update functionality and fix issues in related modules

--- pages/admin/manage/applicants/profile/update/index.jsx
+++ pages/admin/manage/applicants/profile/update/index.jsx
@@ -4,12 +4,9 @@ import { FormLayout } from "@/containers";
 import { ApplicantForm } from "@/components";
 
 const UpdateApplicant = () => {
-  // const [userid, setUserid] = useState("");
-
   const userid = localStorage.getItem("selected-applicantId");
   useEffect(() => {
     console.log("HI, Debargha", localStorage.getItem("selected-applicantId"));
-    // console.log("Debargha", userid);
   }, []);
 
   return (
