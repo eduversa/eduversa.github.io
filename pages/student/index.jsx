dd960de569fc20144f0f1c0f47de083e4c1b892e
Ankur Halder <ankur.halder12345@gmail.com>
2024-04-14 12:43:25 +0530

    Refactor student index page to fetch and display single student data

--- pages/student/index.jsx
+++ pages/student/index.jsx
@@ -1,13 +1,46 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { StudentLayout } from "@/layout";
-function index() {
+import { fetchSingleStudent } from "@/functions";
+import { AllLoader } from "@/components";
+function Index() {
+  const [loading, setLoading] = useState(true);
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect(() => {
+    const userType = localStorage.getItem("userType");
+
+    if (userType === "student") {
+      const studentId = localStorage.getItem("userid");
+
+      const fetchData = async () => {
+        try {
+          setLoading(true);
+          const response = await fetchSingleStudent(studentId);
+
+          if (!response || response.error) {
+            console.error("Error fetching student data:", response?.error);
+            setLoading(false);
+            return;
+          }
+
+          setProfileData(response);
+          setLoading(false);
+        } catch (error) {
+          console.error("Error fetching student data:", error.message);
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }
+  }, []);
+
   return (
     <Fragment>
-      <StudentLayout>
-        <h1>Student Dashboard</h1>
-      </StudentLayout>
+      {loading && <AllLoader />}
+      <StudentLayout>{JSON.stringify(profileData)}</StudentLayout>
     </Fragment>
   );
 }
 
-export default index;
+export default Index;
