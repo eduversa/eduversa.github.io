97d2e93653e92b87144b068ee1fb61a472e2909b
Ankur Halder <ankur.halder12345@gmail.com>
2024-11-02 20:52:56 +0530

    fix: Update comment syntax in applicant index file for clarity

--- pages/applicant/index.jsx
+++ pages/applicant/index.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { ApplicantLayout } from "@/layout";
-// import { getSingleApplicantApi } from "@/functions";
 import { AllLoader } from "@/components";
 import Image from "next/image";
 import { withLoading, devLog, apiRequest } from "@/utils/apiUtils";
@@ -157,7 +156,7 @@ function renderFields(data, parentKey = "") {
         "contact",
         // % H3
         "guardian",
-        // & H4
+        // * H4
         "office_address",
         "first_name",
         "middle_name",
