f9a80361df402e8f7f89a59e88adc68b5e5e6091
Ankur Halder <ankur.halder12345@gmail.com>
2024-11-09 20:46:33 +0530

    feat: Enhance faculty data fetching with improved error handling and redirection

--- pages/faculty/index.jsx
+++ pages/faculty/index.jsx
@@ -4,17 +4,24 @@ import { AllLoader } from "@/components";
 import { withLoading, devLog, apiRequest } from "@/utils/apiUtils";
 import { useAlert } from "@/contexts/AlertContext";
 import { useRouter } from "next/router";
+
 function Faculty() {
   const [loading, setLoading] = useState(false);
   const { showAlert } = useAlert();
   const effectRun = useRef(false);
   const router = useRouter();
+
   useEffect(() => {
     const facultyId = localStorage.getItem("userid");
     const authToken = localStorage.getItem("authToken");
+
     if (effectRun.current) return;
     effectRun.current = true;
 
+    const redirectToHome = () => {
+      localStorage.clear();
+      router.push("/");
+    };
     const fetchFacultyData = async () => {
       const wrappedApiRequest = withLoading(
         apiRequest,
@@ -30,20 +37,20 @@ function Faculty() {
           authToken,
           "GetSinglefaculty"
         );
-        if (!response.success || !response.status) {
+        if (!response?.success || !response?.status) {
           devLog("Error in fetching single faculty data:", response);
-          showAlert(response.message || "Failed to fetch faculty data");
-          localStorage.clear();
-          router.push("/");
+          showAlert(response?.message || "Failed to fetch faculty data");
+          redirectToHome();
+          setFacultyData(response.data.data);
           return;
         }
       } catch (error) {
         devLog("Error in fetching single faculty data:", error);
         showAlert(error.message || "Failed to fetch faculty data");
-        localStorage.clear();
-        router.push("/");
+        redirectToHome();
       }
     };
+
     fetchFacultyData();
   }, [router, showAlert]);
 
