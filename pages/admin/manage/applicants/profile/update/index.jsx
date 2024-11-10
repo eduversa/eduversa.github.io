80495f59a7f3bdf22721128c7596bc63bd2f44a7
debargha10000 <debargha10000@gmail.com>
2024-11-10 22:27:47 +0530

    fixed applicant update form in admin

--- pages/admin/manage/applicants/profile/update/index.jsx
+++ pages/admin/manage/applicants/profile/update/index.jsx
@@ -1,22 +1,47 @@
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { AdminLayout } from "@/layout";
 import { ApplicantForm } from "@/components";
 import { devLog } from "@/utils/apiUtils";
+import { getSingleApplicantApi } from "@/functions";
 
 const UpdateApplicant = () => {
-  useEffect(() => {
-    devLog("HI, Debargha", localStorage.getItem("selected-applicantId"));
-  }, []);
-
   const userid =
     typeof window !== "undefined"
       ? localStorage.getItem("selected-applicantId")
       : null;
 
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const userType = localStorage.getItem("userType");
+    
+    if (userType === "admin" && userid) {
+      const fetchData = async () => {
+        try {
+          setLoading(true);
+          const response = await getSingleApplicantApi(userid);  
+          if (response.status === false) {
+            alert(response.message);
+            setLoading(false);
+            return;
+          }
+          setUserData(response.data);
+          localStorage.setItem("applicant_profile", JSON.stringify(response.data));
+          setLoading(false);
+        } catch (error) {
+          console.error("Error fetching applicant data:", error.message);
+        }
+      };
+      fetchData();
+    }
+  }, [userid]); 
+
   return (
     <Fragment>
       <AdminLayout>
-        <ApplicantForm userid={userid} />
+        <ApplicantForm userid={userid} userData={userData}/>
       </AdminLayout>
     </Fragment>
   );
