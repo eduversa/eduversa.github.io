f1d3743e669e8359f099d2dd473774d72cec41fa
Ankur Halder <ankur.halder12345@gmail.com>
2024-11-10 08:03:45 +0530

    feat: Add contact page for faculty with ContactUs component

---
+++ pages/faculty/contact/index.jsx
@@ -0,0 +1,15 @@
+import { FacultyLayout } from "@/layout";
+import React, { Fragment } from "react";
+import { ContactUs } from "@/containers";
+
+const Contact = () => {
+  return (
+    <Fragment>
+      <FacultyLayout>
+        <ContactUs></ContactUs>
+      </FacultyLayout>
+    </Fragment>
+  );
+};
+
+export default Contact;
