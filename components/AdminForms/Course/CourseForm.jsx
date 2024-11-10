133a3621a9bd464d9ffe89affe4b9a40f8f307a8
Ankur Halder <ankur.halder12345@gmail.com>
2024-11-10 14:44:34 +0530

    refactor: Improve code formatting and consistency in StreamForm and CourseForm components

--- components/AdminForms/Course/CourseForm.jsx
+++ components/AdminForms/Course/CourseForm.jsx
@@ -4,12 +4,12 @@ import { Hourglass, HourglassIcon, Library, User, Wallet } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
-export default function CourseForm({onSubmit,onCancel,currentData}) {
+export default function CourseForm({ onSubmit, onCancel, currentData }) {
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors,isSubmitting },
+    formState: { errors, isSubmitting },
   } = useForm({
     defaultValues: {
       name: "",
@@ -20,58 +20,88 @@ export default function CourseForm({onSubmit,onCancel,currentData}) {
   });
 
   useEffect(() => {
-    if(currentData) reset(currentData);
-  },[currentData])
+    if (currentData) reset(currentData);
+  }, [currentData, reset]);
 
-  return(
+  return (
     <form onSubmit={handleSubmit(onSubmit)} className="form">
-        <div className="form-container">
-            <h3 className="form__title">Add New Course</h3>
+      <div className="form-container">
+        <h3 className="form__title">Add New Course</h3>
 
-            <FormField label={"Course name"} 
-            icon={Library}
-            error={errors?.name?.message} >
-                <input {...register("name",{
-                    required : "Course  name is required" 
-                })}  className="form__input" placeholder="Bachelors in AI and DS"/>
-            </FormField>
+        <FormField
+          label={"Course name"}
+          icon={Library}
+          error={errors?.name?.message}
+        >
+          <input
+            {...register("name", {
+              required: "Course  name is required",
+            })}
+            className="form__input"
+            placeholder="Bachelors in AI and DS"
+          />
+        </FormField>
 
-            <FormField label = {"Fees"} 
-            icon = {HourglassIcon} 
-            error = {errors?.fees?.message}>
-                <input {...register("fees",{
-                    required : "Fees is required"
-                })} className="form__input" placeholder="70,000"/>
-            </FormField>
-            
+        <FormField
+          label={"Fees"}
+          icon={HourglassIcon}
+          error={errors?.fees?.message}
+        >
+          <input
+            {...register("fees", {
+              required: "Fees is required",
+            })}
+            className="form__input"
+            placeholder="70,000"
+          />
+        </FormField>
 
-            
-             <FormField label={"Duration"} 
-            icon={Hourglass}
-            error={errors?.duration?.message} >
-                <input {...register("duration",{
-                    required : "Duration is required" 
-                })}  className="form__input" placeholder="4 years"/>
-            </FormField>
+        <FormField
+          label={"Duration"}
+          icon={Hourglass}
+          error={errors?.duration?.message}
+        >
+          <input
+            {...register("duration", {
+              required: "Duration is required",
+            })}
+            className="form__input"
+            placeholder="4 years"
+          />
+        </FormField>
 
-             <FormField label={"Total Seats"} 
-            icon={User}
-            error={errors?.total_seats?.message} >
-                <input {...register("total_seats",{
-                    required : "Total seats is required" 
-                })}  className="form__input" placeholder="120"/>
-            </FormField>
+        <FormField
+          label={"Total Seats"}
+          icon={User}
+          error={errors?.total_seats?.message}
+        >
+          <input
+            {...register("total_seats", {
+              required: "Total seats is required",
+            })}
+            className="form__input"
+            placeholder="120"
+          />
+        </FormField>
 
-                <div className="form__actions">
-                    <button type="submit" disabled={isSubmitting} className="button button--primary button--icon">
-                        Create Course
-                        </button>
+        <div className="form__actions">
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="button button--primary button--icon"
+          >
+            Create Course
+          </button>
 
-                        <button type="button" className="button button--secondary " onClick={onCancel}>
-                        Cancel
-                        </button>
-                </div>
+          <button
+            type="button"
+            className="button button--secondary "
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
         </div>
-        </form>
-  )
+      </div>
+    </form>
+  );
 }
--- components/AdminForms/Room/RoomForm.jsx
+++ components/AdminForms/Room/RoomForm.jsx
@@ -1,10 +1,10 @@
 import FormField from "@/components/FormField";
 import { devLog } from "@/utils/apiUtils";
-import { BookOpen, Library,Type, Building2 } from "lucide-react";
+import { BookOpen, Library, Type, Building2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
-function RoomForm({onSubmit,onCancel,currentData}) {
+function RoomForm({ onSubmit, onCancel, currentData }) {
   const {
     register,
     handleSubmit,
@@ -20,10 +20,10 @@ function RoomForm({onSubmit,onCancel,currentData}) {
       capacity: "",
     },
   });
- 
+
   useEffect(() => {
-    if(currentData) reset(currentData);
-  },[currentData])
+    if (currentData) reset(currentData);
+  }, [currentData, reset]);
 
   const buildingNos = ["B1", "B2", "B3"];
   const roomTypes = [
@@ -34,73 +34,85 @@ function RoomForm({onSubmit,onCancel,currentData}) {
     "Classroom",
     "Auditorium",
   ];
-  return <>
-       <form onSubmit={handleSubmit(onSubmit)} className="form">
-              <div className="form-container">
-                <h3 className="form__title">{currentData ? "Edit Room" : "Create New Room"}</h3>
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <div className="form-container">
+          <h3 className="form__title">
+            {currentData ? "Edit Room" : "Create New Room"}
+          </h3>
 
-                <FormField
-                  label={"Room Name"}
-                  icon={Library}
-                  error={errors.name?.message}
-                >
-                  <input
-                    {...register("name", {
-                      required: "Room name is required",
-                      minLength: {
-                        value: 1,
-                        message: "Atleast 1 character",
-                      },
-                    })}
-                    className="form__input"
-                    placeholder="e.g., LG 3.1"
-                  />
-                </FormField>
-             
-                    
-                    <div className="form__group">
-                    <label className="form__label">
-                    <Building2 size={16} />
-                      Building
-                      </label>
+          <FormField
+            label={"Room Name"}
+            icon={Library}
+            error={errors.name?.message}
+          >
+            <input
+              {...register("name", {
+                required: "Room name is required",
+                minLength: {
+                  value: 1,
+                  message: "Atleast 1 character",
+                },
+              })}
+              className="form__input"
+              placeholder="e.g., LG 3.1"
+            />
+          </FormField>
 
-                      <select {...register("building",{
-                        required : "building is required" 
-                      })}  >
-                        <option value="" disabled selected hidden>Select a building</option>
-                        {buildingNos.map((c,i) =>(
-                          <option key={i} value={c}>
-                            {c}
-                          </option>
-                        ))}
-                      </select>
-                      {errors.building?.message && <p className="form__error">{errors.building.message}</p>}
-                    </div>
+          <div className="form__group">
+            <label className="form__label">
+              <Building2 size={16} />
+              Building
+            </label>
 
-                    <div className="form__group">
-                    <label className="form__label">
-                    <BookOpen size={16} />
-                      Type of room
-                      </label>
+            <select
+              {...register("building", {
+                required: "building is required",
+              })}
+            >
+              <option value="" disabled selected hidden>
+                Select a building
+              </option>
+              {buildingNos.map((c, i) => (
+                <option key={i} value={c}>
+                  {c}
+                </option>
+              ))}
+            </select>
+            {errors.building?.message && (
+              <p className="form__error">{errors.building.message}</p>
+            )}
+          </div>
 
-                      <select {...register("type",
-                        
-                      {
-                        required : "Type is required" 
-                      } 
-                      
-                      )}  >
-                        <option value="" >Select the type of room</option>
-                        {roomTypes.map((c,i) =>(
-                          <option key={i} value={c}>
-                            {c}
-                          </option>
-                        ))}
-                      </select>
-                      {errors.type?.message && <p className="form__error">{errors.type.message}</p>}
-                    </div>
+          <div className="form__group">
+            <label className="form__label">
+              <BookOpen size={16} />
+              Type of room
+            </label>
 
-                {/* <FormField
+            <select
+              {...register(
+                "type",
+
+                {
+                  required: "Type is required",
+                }
+              )}
+            >
+              <option value="">Select the type of room</option>
+              {roomTypes.map((c, i) => (
+                <option key={i} value={c}>
+                  {c}
+                </option>
+              ))}
+            </select>
+            {errors.type?.message && (
+              <p className="form__error">{errors.type.message}</p>
+            )}
+          </div>
+
+          {/* <FormField
                   label="Stream"
                   icon={GitBranch}
                   error={errors.stream?.message}
@@ -114,27 +126,42 @@ function RoomForm({onSubmit,onCancel,currentData}) {
                   />
                 </FormField> */}
 
-                    <FormField label={"Room Capacity"} icon={Type} error={errors.capacity?.message}>
-                        <input {...register("capacity",{
-                            required : "Capacity of room is required",
-                        })}
-                        className="form__input" placeholder="60/70 members"
-                        />
-                    </FormField>
-                    <div className="form__actions">
-                        <button type="submit" disabled={isSubmitting} className="button button--primary button--icon">
-                            {currentData ? "Confirm Edit" : "Create Room"}
-                        </button>
+          <FormField
+            label={"Room Capacity"}
+            icon={Type}
+            error={errors.capacity?.message}
+          >
+            <input
+              {...register("capacity", {
+                required: "Capacity of room is required",
+              })}
+              className="form__input"
+              placeholder="60/70 members"
+            />
+          </FormField>
+          <div className="form__actions">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="button button--primary button--icon"
+            >
+              {currentData ? "Confirm Edit" : "Create Room"}
+            </button>
 
-                        {onCancel && (
-                            <button type="button" onClick={onCancel} className="button button--secondary">
-                                Cancel
-                            </button>
-                        )}
-                    </div>
-              </div>
-            </form>
-  </>;
+            {onCancel && (
+              <button
+                type="button"
+                onClick={onCancel}
+                className="button button--secondary"
+              >
+                Cancel
+              </button>
+            )}
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default RoomForm;
