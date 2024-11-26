a78691e0b07a489f24ab3418cd10d8ea756e72f1
Ankur Halder <ankur.halder12345@gmail.com>
2024-11-26 16:36:01 +0530

    feat: Implement user data fetching based on selected user type with loading state

--- pages/admin/manage/users/index.jsx
+++ pages/admin/manage/users/index.jsx
@@ -1,15 +1,86 @@
-import React, { useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import { AllLoader } from "@/components";
+import { useAlert } from "@/contexts/AlertContext";
+import { withLoading, devLog, apiRequest } from "@/utils/apiUtils";
 
 const Users = () => {
   const userTypes = ["Applicant", "Student", "Faculty"];
-  const [userType, setUserType] = useState(userTypes[0]);
-
+  const [userType, setUserType] = useState(userTypes[0].toLowerCase());
+  const [loading, setLoading] = useState(true);
+  const year = new Date().getFullYear();
+  const { showAlert } = useAlert();
   const handleChange = (e) => {
     setUserType(e.target.value);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const authToken = localStorage.getItem("authToken");
+      const wrappedApiRequest = withLoading(
+        apiRequest,
+        setLoading,
+        showAlert,
+        "FetchData"
+      );
+
+      try {
+        let userResponse;
+
+        if (userType === "faculty") {
+          userResponse = await wrappedApiRequest(
+            `/faculty/all`,
+            "GET",
+            null,
+            authToken
+          );
+        } else if (userType === "applicant") {
+          userResponse = await wrappedApiRequest(
+            `/applicant/year?year=${year}`,
+            "GET",
+            null,
+            authToken
+          );
+        } else if (userType === "student") {
+          userResponse = await wrappedApiRequest(
+            `/student/find/all`,
+            "GET",
+            null,
+            authToken
+          );
+        }
+
+        if (!userResponse.success) {
+          devLog("Error fetching users:", userResponse.message);
+          showAlert(userResponse.message || "Error fetching users.");
+          return;
+        }
+        // setUsers(userResponse.data.data);
+
+        const collegeResponse = await wrappedApiRequest(
+          `/college/?college_id=304`,
+          "GET",
+          null,
+          authToken
+        );
+        if (!collegeResponse.success) {
+          devLog("Error fetching college details:", collegeResponse.message);
+          showAlert(
+            collegeResponse.message || "Error fetching college details."
+          );
+          return;
+        }
+        // setCollegeData(collegeResponse.data.data);
+      } catch (error) {
+        devLog("Error fetching data:", error.message);
+        showAlert(error.message || "Failed to fetch data.");
+      }
+    };
+
+    fetchData();
+  }, [userType, showAlert, year]);
   return (
-    <div>
+    <Fragment>
+      {loading && <AllLoader />}
       <h1>
         <select value={userType} onChange={handleChange}>
           {userTypes.map((type, index) => (
@@ -19,7 +90,7 @@ const Users = () => {
           ))}
         </select>
       </h1>
-    </div>
+    </Fragment>
   );
 };
 
